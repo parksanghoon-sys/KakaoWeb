d5dc710f8b68607d9c0c589bdfbd9019dcc24cc4
DESKTOP-L8PQMEN\User <power93266@naver.com>
2023-04-08 18:15:54 +0900

    초기 커밋.

---
+++ KakaoWeb/Server/Hubs/ChatHub.cs
@@ -0,0 +1,37 @@
+﻿using KakaoWeb.Server.Data;
+using KakaoWeb.Shared;
+using KakaoWeb.Shared.Models;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.EasyUse.Server;
+
+namespace KakaoWeb.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ChatHub(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task SyncFriends(RequestInfo request)
+        {
+            List<FriendsModel> friends = new();
+            foreach (var user in _context.Users.ToList())
+            {
+                friends.Add(new FriendsModel() {
+                    Id = user.Id, 
+                    Email = user.Email, 
+                    Name = user.UserName 
+                });
+            }
+
+            ResponseFriendsPack pack = new();
+            pack.Friends = friends;
+
+            await Clients.All.SendAsync(pack);
+        }
+
+
+    }
+}
---
+++ KakaoWeb/Shared/Models/FriendsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace KakaoWeb.Shared.Models
+{
+    public class FriendsModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
